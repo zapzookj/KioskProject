52b024381f3425a33f4b98e550ea2250f8664762
zapzookj <zapzook@naver.com>
2024-01-09 19:33:20 +0900

    .

---
+++ Order.java
@@ -0,0 +1,17 @@
+package KioskProject;
+import java.util.ArrayList;
+import java.util.List;
+public class Order{
+    String name;
+    String description;
+    int price;
+    int quantity;
+
+    Order(String name, int price, int quantity, String description){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}
